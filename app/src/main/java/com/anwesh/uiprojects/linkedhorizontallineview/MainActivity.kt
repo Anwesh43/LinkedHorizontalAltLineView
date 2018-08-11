361b53c9734cecb5eac5496bf6a8df7bb1c2aedb
Anwesh43 <anweshthecool0@gmail.com>
2018-08-11 17:33:11 +0530

    adding AnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhorizontallineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhorizontallineview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedhorizontallineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.horizontallineview.HorizontalLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        HorizontalLineView.create(this)
+        val view : HorizontalLineView = HorizontalLineView.create(this)
         fullScreen()
+        view.addAnimationListener({createToast("animation ${it} completed")}, {createToast("animation ${it} reset")})
+    }
+
+    fun createToast(msg : String) {
+        Toast.makeText(this, msg,Toast.LENGTH_SHORT).show()
     }
 }
 
