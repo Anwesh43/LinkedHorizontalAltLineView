d5ae18dde6eed27b1641bc32063fbc83bcf227ea
Anwesh43 <anweshthecool0@gmail.com>
2018-08-11 05:59:49 +0530

    adding HLNode class

--- horizontallineview/src/main/java/com/anwesh/uiprojects/horizontallineview/HorizontalLineView.kt
+++ horizontallineview/src/main/java/com/anwesh/uiprojects/horizontallineview/HorizontalLineView.kt
@@ -92,4 +92,49 @@ class HorizontalLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HLNode(var i : Int, val state : State = State()) {
+
+        private var next : HLNode? = null
+
+        private var prev : HLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HLNode {
+            var curr : HLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
