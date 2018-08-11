3a56a66652294098fa151613a12c3d41d4311d04
Anwesh43 <anweshthecool0@gmail.com>
2018-08-11 05:37:13 +0530

    adding State class

--- horizontallineview/src/main/java/com/anwesh/uiprojects/horizontallineview/HorizontalLineView.kt
+++ horizontallineview/src/main/java/com/anwesh/uiprojects/horizontallineview/HorizontalLineView.kt
@@ -28,4 +28,24 @@ class HorizontalLineView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            this.scale += 0.1f * this.dir
+            if (Math.abs(this.scale - this.prevScale) > 1) {
+                this.scale = this.prevScale + this.dir
+                this.dir = 0f
+                this.prevScale = this.scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
