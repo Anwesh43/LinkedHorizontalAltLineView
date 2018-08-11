753acb715d4ac1ca288d633e1275624f503ebd33
Anwesh43 <anweshthecool0@gmail.com>
2018-08-11 06:30:15 +0530

    adding LinkedHorizontalView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhorizontallineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhorizontallineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhorizontallineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.horizontallineview.HorizontalLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HorizontalLineView.create(this)
     }
 }
