7f227461cea799546e9039a6b97b5ab99cf6a7b8
Anwesh43 <anweshthecool0@gmail.com>
2018-08-11 05:30:25 +0530

    importing libraries and defining nodes

--- horizontallineview/src/main/java/com/anwesh/uiprojects/horizontallineview/HorizontalLineView.kt
+++ horizontallineview/src/main/java/com/anwesh/uiprojects/horizontallineview/HorizontalLineView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.horizontallineview
 /**
  * Created by anweshmishra on 11/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
